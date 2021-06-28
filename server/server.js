c0bda8fb89ee2f4afe7c6d6ff6a6a6e0b17fcf6b
Divine Orji <dpkreativ@gmail.com>
2021-06-28 11:20:17 +0100

    configure fastify server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const app = require("fastify")({ logger: true });
+
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send({ hello: "Welcome" });
+});
+
+app.listen(process.env.PORT, (err, addr) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+
+  app.log.info(`Your server is listening on port ${process.env.PORT}`);
+});
