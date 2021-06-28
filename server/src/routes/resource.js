7924e4ea57631682b44f876c38547571bfa8e39e
Divine Orji <dpkreativ@gmail.com>
2021-06-28 11:45:49 +0100

    build basic routes

--- server/src/routes/resource.js
+++ server/src/routes/resource.js
@@ -1,7 +1,19 @@
 const resourceController = require("../controllers/resourceController");
 
 const routes = async (app, opts) => {
-  // code
+  // Get all resources
+  app.route({
+    method: "GET",
+    url: "/resources",
+    handler: resourceController.getResources,
+  });
+
+  // Add new resource
+  app.route({
+    method: "POST",
+    url: "/resources",
+    handler: resourceController.addResource,
+  });
 };
 
 module.exports = routes;
