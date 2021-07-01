a5e9e8501c00a8ae20d4f7f37e0fed6399240485
Divine Orji <dpkreativ@gmail.com>
2021-07-01 16:37:18 +0100

    add more styles to button

--- client/src/components/button/Button.js
+++ client/src/components/button/Button.js
@@ -3,6 +3,11 @@ import styled from "styled-components";
 
 const CustomButton = styled.button`
   padding: 1rem;
+  cursor: pointer;
+  background-color: var(--primary-color);
+  color: white;
+  border: 2px solid var(--primary-color);
+  border-radius: 1rem;
 `;
 
 const Button = ({ children }) => {
