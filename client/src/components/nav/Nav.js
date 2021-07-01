6c1857ab6fa4017e286b2f5a58900634fe8b302f
Divine Orji <dpkreativ@gmail.com>
2021-07-01 16:28:31 +0100

    custom Nav and style

--- client/src/components/nav/Nav.js
+++ client/src/components/nav/Nav.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const CustomNav = styled.nav`
+  padding: 1rem;
+
+  h1 {
+    color: #021e1e;
+  }
+`;
 
 const Nav = () => {
-  return <div>Devsources</div>;
+  return (
+    <CustomNav>
+      <h1>Devsources</h1>
+    </CustomNav>
+  );
 };
 
 export default Nav;
