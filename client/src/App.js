01c7f4756879356f03a3a83962fd27ffd18765c8
Divine Orji <dpkreativ@gmail.com>
2021-07-01 16:33:56 +0100

    import button component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import Button from "./components/button/Button";
 import Nav from "./components/nav/Nav";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <section>
+        <Button>Suggest Resource</Button>
+      </section>
     </div>
   );
 }
