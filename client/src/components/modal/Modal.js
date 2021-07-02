8c7ee1b19d0281f9f66a8e782d0ff3cdc4bb15c6
Divine Orji <dpkreativ@gmail.com>
2021-07-02 04:14:15 +0100

    add margin to modal

--- client/src/components/modal/Modal.js
+++ client/src/components/modal/Modal.js
@@ -17,6 +17,7 @@ const Background = styled.div`
 const ModalWrapper = styled.div`
   width: 800px;
   height: 500px;
+  margin: 1rem;
   box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
   background: #fff;
   color: #000;
