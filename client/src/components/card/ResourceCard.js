7ef09bb30fead19f06c7b231f4b7d8d95d7031c8
Divine Orji <dpkreativ@gmail.com>
2021-07-02 03:20:53 +0100

    add classNames for content and tweak css

--- client/src/components/card/ResourceCard.js
+++ client/src/components/card/ResourceCard.js
@@ -1,17 +1,22 @@
 import React from "react";
 import styled from "styled-components";
-
 const CustomCard = styled.div`
   background-color: white;
   border-radius: 1rem;
-  padding: 3rem;
+  padding: 2rem;
+
+  .title,
+  .description {
+    margin-bottom: 0.5rem;
+    color: var(--secondary-color);
+  }
 `;
 
 const ResourceCard = ({ title, description, link }) => {
   return (
     <CustomCard>
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <h3 className="title">{title}</h3>
+      <p className="description">{description}</p>
       <a href={`${link}`}>
         <p>{link}</p>
       </a>
